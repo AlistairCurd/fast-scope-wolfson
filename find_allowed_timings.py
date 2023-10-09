444384f1a860ac1b961138d1f9455358382fc981
Alistair Curd <a.curd@leeds.ac.uk>
2023-10-09 16:40:06 +0100

    Start to set up frame rate test - incomplete.

--- find_allowed_timings.py
+++ find_allowed_timings.py
@@ -1,76 +1,49 @@
 """Find the frame rates allowed at a given resolution."""
 
 from egrabber import EGenTL, EGrabber, errors
-import argparse
 import sys
+import numpy as np
+import set_grabber_properties
 
 
-# Create grabber
-gentl = EGenTL()
-grabber = EGrabber(gentl)
-
-print('\nGrabber created.')
-
-
-def get_cmd_inputs(allowed_roi_widths=[128, 256, 384, 512, 640, 768, 896,
-                                       1024, 1152, 1280
-                                       ],
-                   max_height=400
-                   ):
-    """Get command prompt inputs for acquisition.
-
+def fps_test(grabber, fps_min_test=1, fps_max_test=10000, fps_step=1):
+    """Test frame rates for whether they produce an error.
+    Output minimum and maximum allowed frame rates.
+    
     Args:
-        allowed_roi_widths (list):
-            List of ROI widths that do not produce an error.
-        max_height (int):
-            Maximum ROI height allowed.
-
-    Returns:
-        args (argparse.Namespace object):
-            Parsed arguments for downstream use.
+        grabber (EGrabber object):
+            Frame grabber object for acquisition.
+        fps_min_test (float):
+            Minimum frame rate to test.
+        fps_max_test (float):
+            Maximum frames rate to test.
+        fps_step (float):
+            Steps between the test fps settings.
     """
-    parser = argparse.ArgumentParser()
+    fps_min_allowed = 1
 
-    parser.add_argument('-n', '--numframes',
-                        dest='n_frames',
-                        type=int,
-                        default=10,
-                        help='Required. Number of frames to acquire.'
-                        )
+    arrived_at_min_fps = False
+    arrived_at_max_fps = False
 
-    parser.add_argument('-W', '--width',
-                        dest='roi_width',
-                        type=int,
-                        default=1280,
-                        help='Optional. Width of ROI in pixels.'
-                        ' Must be in [128, 256, 384, 512, 640, 768, 896,'
-                        ' 1024, 1152, 1280].'
-                        )
+    for fps in np.arange(fps_min_test, fps_max_test, fps_step):
+        while arrived_at_min_fps is False:
+            try:
+                grabber.remote.set('AquisitionFrameRate', fps)
+            except errors.GenTLException:
+                continue
+            arrived_at_min_fps = True
 
-    # Change default if different number of output banks in use?
-    parser.add_argument('-H', '--height',
-                        dest='roi_height',
-                        type=int,
-                        default=400,
-                        help='Optional. Height of ROI in pixels.'
-                        ' Must be <= 400.'
-                        )
 
-    args = parser.parse_args()
+def main():
+    # Get user settings
+    user_settings = set_grabber_properties.get_cmd_inputs()
+    fps = user_settings.fps
+    exp_time = user_settings.exp_time
 
-    # Check whether height and width will be allowed
-    stop = False
+    # Make sure exposure time is less than cycling time
+    # Set if not given
+    exp_time = set_grabber_properties.check_and_set_exposure(fps, exp_time)
 
-    if args.roi_height > max_height:
-        print('\nPlease choose an ROI height <= 400 pixels.')
-        stop = True
-
-    if args.roi_width not in allowed_roi_widths:
-        print('\nPlease choose one of these options for ROI width:'
-              '\n128, 256, 384, 512, 640, 768, 896, 1024, 1152, 1280')
-        stop = True
-
-    if stop is True:
-        sys.exit()
-
-    return args
+    # Create grabber
+    gentl = EGenTL()
+    grabber = EGrabber(gentl)
