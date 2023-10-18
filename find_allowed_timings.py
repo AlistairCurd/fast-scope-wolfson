e541162ea8618b127ad20033f7fc06a8e9934db6
Alistair Curd <a.curd@leeds.ac.uk>
2023-10-18 10:57:26 +0100

    Use cmd inputs.

--- find_allowed_timings.py
+++ find_allowed_timings.py
@@ -1,9 +1,10 @@
 """Find the frame rates allowed at a given resolution."""
 
 from egrabber import EGenTL, EGrabber, errors
+import argparse
 import numpy as np
 import time
-# import sys
+import sys
 import set_grabber_properties
 
 
@@ -59,11 +60,11 @@ def fps_test(grabber, fps_min_test=1, fps_max_test=10000, fps_step=1):
 
     # PRINT RESULTS if extrema found
     if fps_min_allowed is not None:
-        print('\nMinimum allowable FPS setting found : {}.'
+        print('\nMinimum allowable FPS setting found: {}.'
               .format(fps_min_allowed)
               )
     if fps_max_allowed is not None:
-        print('\nMaximum allowable FPS setting found : {}.'
+        print('Maximum allowable FPS setting found: {}.'
               .format(fps_max_allowed)
               )
 
@@ -84,37 +85,120 @@ def fps_test(grabber, fps_min_test=1, fps_max_test=10000, fps_step=1):
               )
 
 
-def main():
-    """Test frame rates for whether they produce an error.
-    Output minimum and maximum allowed frame rates.
+def get_cmd_inputs(allowed_roi_widths=[128, 256, 384, 512, 640, 768, 896,
+                                       1024, 1152, 1280
+                                       ],
+                   max_height=400
+                   ):
+    """Get command prompt inputs for acquisition.
 
     Args:
-        self (EGrabber object):
-            Frame grabber object for acquisition.
-        fps_min_test (float):
-            Minimum frame rate to test.
-        fps_max_test (float):
-            Maximum frame rate to test.
-        fps_step (float):
-            Steps between the test fps settings.
+        allowed_roi_widths (list):
+            List of ROI widths that do not produce an error.
+        max_height (int):
+            Maximum ROI height allowed.
+
+    Returns:
+        args (argparse.Namespace object):
+            Parsed arguments for downstream use.
+    """
+# Include defaults in help text
+    parser = argparse.ArgumentParser(
+        description="Test for minimum and maximum frame rates"
+        " at chosen ROI dimensions.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+
+    parser.add_argument('-W', '--width',
+                        dest='roi_width',
+                        type=int,
+                        default=1280,
+                        help='Width of ROI in pixels.'
+                        ' Must be in {}.'.format(allowed_roi_widths)
+                        )
+
+    # Change default if different number of output banks in use?
+    parser.add_argument('-H', '--height',
+                        dest='roi_height',
+                        type=int,
+                        default=400,
+                        help='Optional. Height of ROI in pixels.'
+                        ' Must be <= {}.'.format(max_height)
+                        )
+
+    parser.add_argument('--min-fps',
+                        dest='fps_min_test',
+                        type=float,
+                        default=1.,
+                        help='Minimum frame rate to test (frames per second).'
+                        )
+
+    parser.add_argument('--max-fps',
+                        dest='fps_max_test',
+                        type=float,
+                        default=100000.,
+                        help='Minimum frame rate to test (frames per second).'
+                        )
+
+    parser.add_argument('--step-fps',
+                        dest='fps_step',
+                        type=float,
+                        default=1,
+                        help='Increment to frame rate during test'
+                        ' (frames per second).'
+                        )
+
+    args = parser.parse_args()
+
+    # Check ROI width and height.
+    # Print messages and exit if incompatible with camera.
+    allowable_width_height = \
+        set_grabber_properties.check_input_width_and_height(
+            args.roi_width, args.roi_height,
+            allowed_roi_widths, max_height
+            )
+
+    if allowable_width_height is False:
+        sys.exit()
+
+    return args
+
+
+def main():
+    """Test eGrabber frame rates for whether they produce an error.
+    Output minimum and maximum allowed frame rates.
     """
     # Get user settings
-    user_settings = set_grabber_properties.get_cmd_inputs()
-    roi_width = user_settings.roi_width
-    roi_height = user_settings.roi_height
+    user_settings = get_cmd_inputs()
+
+    # Display settings
+    print('')
+    print('Image width: ', user_settings.roi_width)
+    print('Image height: ', user_settings.roi_height)
+    print('Minimum FPS to test: ', user_settings.fps_min_test)
+    print('Maximum FPS to test: ', user_settings.fps_max_test)
+    print('FPS increment for test: ', user_settings.fps_step)
 
     # Create grabber
     gentl = EGenTL()
     grabber = EGrabber(gentl)
 
     # Set up ROI
-    set_grabber_properties.set_roi(grabber, width=roi_width, height=roi_height)
+    set_grabber_properties.set_roi(grabber,
+                                   width=user_settings.roi_width,
+                                   height=user_settings.roi_height
+                                   )
 
     # Set up grabber stream for unscrambled images
-    set_grabber_properties.unscramble_phantom_S710_output(grabber, roi_width)
+    set_grabber_properties.unscramble_phantom_S710_output(
+        grabber, user_settings.roi_width
+        )
 
     # Test allowed frame rates
-    fps_test(grabber, fps_min_test=1, fps_max_test=100000, fps_step=1)
+    fps_test(grabber,
+             fps_min_test=user_settings.fps_min_test,
+             fps_max_test=user_settings.fps_max_test,
+             fps_step=user_settings.fps_step)
 
 
 if __name__ == '__main__':
