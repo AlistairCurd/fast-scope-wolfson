2914c3077215cfc1698df20b58cff98ea3b3c5f0
Alistair Curd <a.curd@leeds.ac.uk>
2023-10-18 11:52:46 +0100

    Remove redundant lines and variables from main.

--- acquire-number-rate-exposure.py
+++ acquire-number-rate-exposure.py
@@ -35,39 +35,26 @@ def set_output_path(output_parent_dir='C://Temp',
 
     output_path.mkdir()
 
-#    try:
-#        output_path.mkdir()
-#    except FileExistsError:
-#        print('\n'
-#              '*** Please choose an output directory '
-#              'that does not already exist and start again.'
-#              )
-#        sys.exit()
-
     return output_path
 
 
 def main():
-    # Get user settings
-    user_settings = set_grabber_properties.get_cmd_inputs()
-    n_frames = user_settings.n_frames
-    fps = user_settings.fps
-    exp_time = user_settings.exp_time
-    roi_width = user_settings.roi_width
-    roi_height = user_settings.roi_height
+    # Get script arguments
+    cmd_args = set_grabber_properties.get_cmd_inputs()
 
     # Make sure exposure time is less than cycling time
     # Set if not given
-    exp_time = set_grabber_properties.check_and_set_exposure(fps, exp_time)
+    exp_time = set_grabber_properties.check_and_set_exposure(cmd_args.fps,
+                                                             cmd_args.exp_time
+                                                             )
 
     # Display settings
-
-    print('\nNumber of frames : {}'.format(n_frames))
-    print('Frames per second : {:.1f}'.format(fps))
-    print('Cycling time : {:.1f}'.format(1e6 / fps), 'us')
+    print('\nNumber of frames : {}'.format(cmd_args.n_frames))
+    print('Frames per second : {:.1f}'.format(cmd_args.fps))
+    print('Cycling time : {:.1f}'.format(1e6 / cmd_args.fps), 'us')
     print('Exposure time :', exp_time, 'us')
-    print('Image width: ', roi_width)
-    print('Image height: ', roi_height)
+    print('Image width: ', cmd_args.roi_width)
+    print('Image height: ', cmd_args.roi_height)
 
     # Set up saving location
     output_path = set_output_path()
@@ -78,24 +65,29 @@ def main():
     grabber = EGrabber(gentl)
 
     # Set up ROI
-    set_grabber_properties.set_roi(grabber, width=roi_width, height=roi_height)
+    set_grabber_properties.set_roi(grabber,
+                                   width=cmd_args.roi_width,
+                                   height=cmd_args.roi_height
+                                   )
 
     # Set up grabber stream for unscrambled images
-    set_grabber_properties.unscramble_phantom_S710_output(grabber, roi_width)
+    set_grabber_properties.unscramble_phantom_S710_output(grabber,
+                                                          cmd_args.roi_width
+                                                          )
 
     # Configure fps and exposure time
-    grabber.remote.set('AcquisitionFrameRate', fps)
+    grabber.remote.set('AcquisitionFrameRate', cmd_args.fps)
     time.sleep(0.001)  # Allow fps to set first
     grabber.remote.set('ExposureTime', exp_time)
 
     # Make a buffer ready for every frame and start
-    grabber.realloc_buffers(n_frames)
-    grabber.start(n_frames)
+    grabber.realloc_buffers(cmd_args.n_frames)
+    grabber.start(cmd_args.n_frames)
 
     # timestamps = []
 
     # Acquire image into buffer and save for every frame
-    for frame in range(n_frames):
+    for frame in range(cmd_args.n_frames):
         buffer = Buffer(grabber)
         # timestamps.append(buffer.get_info(cmd=3, info_datatype=8))
         # timestamp = buffer.get_info(cmd=3, info_datatype=8)
